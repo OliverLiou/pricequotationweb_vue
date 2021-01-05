6cae6304d2e65c53790f30a98dcb1b9400e2ba71
OliverLiou <47354638+OliverLiou@users.noreply.github.com>
2021-01-05 17:04:19 +0800

    2020/01/05

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,67 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
-    <!-- <b-form-file 
-      v-model="file1"
-      :state="Boolean(file1)"
-      placeholder="Choose a file or drop it here..."
-      drop-placeholder="Drop file here..."
-      ></b-form-file> -->
-      <div>
+    <div>
       <b-form-group label="Bom表(Excel):" label-cols-sm="1" label-size="sm">
-      <b-form-file id="file-small" size="sm"></b-form-file>
+        <b-form-file
+          id="file-small"
+          size="sm"
+          v-model="file"
+          :state="Boolean(file)"
+        ></b-form-file>
+        <b-button @click="test"> 上傳 </b-button>
       </b-form-group>
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
+
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      file: "",
+    };
+  },
   components: {
     // HelloWorld
-  }
-}
+  },
+  methods: {
+    test() {
+      let formData = new FormData();
+      formData.append("file", this.file);
+      axios
+        .post(
+          "http://localhost:5001/api/Bom/CreateBom",
+          formData,
+          {
+            Headers: { "Content-Type": "multipart/form-data" }
+          }
+        )
+        .then(function () {
+          this.file = this.$refs.file.files[0];
+        })
+        .catch(function (error) {
+          alert(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
